24d0aa1a1a17f19cf648d22a34bb02011a263259
Daniel Olvio <daniel-olivo@outlook.com>
2018-02-10 06:02:29 +0000

    Delete MenuPage.js

--- Incomplete MenuPage/MenuPage.js
+++
@@ -1,6 +0,0 @@
-setInterval(myTimer, 1000);
-
-function myTimer() {
-    var currentDate = new Date();
-    document.getElementById("time").innerHTML = currentDate.toDateString() + " " +  currentDate.toLocaleTimeString();
-}
