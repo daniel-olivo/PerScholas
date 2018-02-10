9cac9fdb3e1a400fe4477799ab403c19db82bddf
Daniel Olvio <daniel-olivo@outlook.com>
2018-02-10 06:03:26 +0000

    Delete LoginPage.js

--- JS/LoginPage.js
+++
@@ -1,6 +0,0 @@
-setInterval(myTimer, 1000);
-
-function myTimer() {
-    var currentDate = new Date();
-    document.getElementById("time").innerHTML = currentDate.toDateString() + "<br>" +  currentDate.toLocaleTimeString();
-}
